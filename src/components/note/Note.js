97fb90516533d0bf459ef29712e38e113ed1a5cc
Rashedul Islam Hridoy <hridoy.developer@yahoo.com>
2021-09-28 22:59:56 +0600

    Design Completed

--- src/components/note/Note.js
+++ src/components/note/Note.js
@@ -3,16 +3,16 @@ import './Note.css'
 
 const Note = () => {
     return (
-        <div className='w-75 d-flex justify-content-between mt-5 mx-auto note'>
-            <div-1>
-                <p>Task Name</p>
-            </div-1>
-            <div-2>
-                <p>Comment</p>
-            </div-2>
-            <div-3>
-            <i class="fas fa-trash"></i>
-            </div-3>
+        <div className='w-75 d-flex justify-content-between mb-3 mt-5 mx-auto note'>
+            <div className='p-1'>
+                <p className='mx-3 text-warning'>Task Name</p>
+            </div>
+            <div className='p-1'>
+                <p className='text-warning'>Comment</p>
+            </div>
+            <div>
+            <i  class="fas fa-trash text-danger"></i>
+            </div>
         </div>
     );
 }
