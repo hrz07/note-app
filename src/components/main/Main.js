721564c0c34c9d8e856d26f80355fca4602a2974
Rashedul Islam Hridoy <hridoy.developer@yahoo.com>
2021-09-29 00:17:50 +0600

    All functional js coding completed

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,17 +1,57 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './Main.css'
 import Note from '../note/Note';
 
 const Main = () => {
+
+
+    const [data,setData] = useState({
+        name : '',
+        comment : ''
+    })
+    const [note,setNote] = useState([])
+
+    const inputHandller = (event)=>{
+        const {value,name} = event.target;
+        setData((previousData)=>{
+            return{
+                ...previousData,
+                [name]: value
+            }
+        })
+    }
+
+    const submitNote = (e) =>{
+        e.preventDefault();
+        setNote([...note,data]);
+        setData({
+            name : '',
+            comment : ''
+        });
+    }
+
+    const deleteNote = (id)=>{
+        let newList = [...note];
+        newList.splice(id,1);
+        setNote(newList);
+       
+    }
+
+    
     return (
         <div className='Main_template'>
+         <p className='text-light' >{note.length>0 ? `You have ${note.length} note ` : `You haven't any note` }</p>  
             <div className='w-50  mx-auto d-flex flex-column'>
-                <input className=' my-2 form-control  mt-5 mb-2' type="text" placeholder='Enter Name...' />
-                <textarea className='form-control mb-2' name="task" id="" cols="30" rows="10" placeholder='Task Name...'></textarea> 
-                <button className='btn btn-success w-25 mb-2'>Add Item</button>            
+                <input onChange={inputHandller} autoComplete='off' name='name' value={data.name} className=' my-2 form-control  mt-5 mb-2' type="text" placeholder='Enter Name...' />
+                <textarea onChange={inputHandller} name='comment' value={data.comment} className='form-control mb-2' id="" cols="30" rows="10" placeholder='Task Name...'></textarea> 
+                <button onClick={submitNote} className='btn btn-success  w-25 my-2'>Add Item</button>            
             </div>
             
-            <Note/>
+            {
+                note.map((dt,index)=>{
+                    return <Note name={dt.name}  comment={dt.comment} key={index} id={index} deleteNote={deleteNote} />
+                })
+            }
         </div>
     );
 }
--- src/components/note/Note.js
+++ src/components/note/Note.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './Note.css'
 
-const Note = () => {
+const Note = ({name,comment,id,deleteNote}) => {
+
+
     return (
-        <div className='w-75 d-flex justify-content-between mb-3 mt-5 mx-auto note'>
+        <div className='w-75 d-flex justify-content-between mb-2 mt-5 mx-auto note'>
             <div className='p-1'>
                 <p className='mx-3 text-warning'>Task Name</p>
+                <p className='text-light' >{name}</p>
             </div>
             <div className='p-1'>
                 <p className='text-warning'>Comment</p>
+                <p className='text-light'  >{comment}</p>
             </div>
             <div>
-            <i  class="fas fa-trash text-danger"></i>
+            <i  onClick={()=>deleteNote(id)} className="fas fa-trash text-danger"></i>
             </div>
         </div>
     );
